5c1b9b0cdf80d462dd05bd3610fbbe54db1b51ea
Joseph Higgins <53231139+JoeLimewire@users.noreply.github.com>
2021-06-28 23:49:55 +0100

    another commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
-const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { UnknownComponent } from './pages/unknown/unknown.component';
+
+const routes: Routes = [
+  {path: 'homeLink', component: HomeComponent},
+  {path: 'aboutLink', component: AboutComponent},
+  {path: 'projectsLink', component: ProjectsComponent},
+  {path: 'contactLink', component: ContactComponent},
+  {path: '**', component: UnknownComponent},
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes),
+    CommonModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
